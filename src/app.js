22f103d0dda2c0957d8657b4dcc1a5a81f629119
Sadiel0 <sadielalmanza@outlook.com>
2021-11-10 01:51:41 +0000

    finished project

--- src/app.js
+++ src/app.js
@@ -6,6 +6,39 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
+  CardGenerator();
   console.log("Hello Rigo from the console!");
+};
+
+let CardGenerator = () => {
+  let cardNumbers = [
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "K",
+    "J",
+    "A"
+  ];
+  let cardSuits = ["♥", "♠", "♣", "♦"];
+  let randomCardNumber =
+    cardNumbers[Math.floor(Math.random() * cardNumbers.length)];
+  let randomCardSuits = cardSuits[Math.floor(Math.random() * cardSuits.length)];
+  let firstSpade = document.querySelector(".first-spade");
+  firstSpade.innerHTML = randomCardSuits;
+  let secondSpade = document.querySelector(".second-spade");
+  secondSpade.innerHTML = randomCardSuits;
+  let number = document.querySelector(".mid");
+  number.innerHTML = randomCardNumber;
+
+  if (randomCardSuits === "♥" || randomCardSuits === "♦") {
+    firstSpade.style.color = "red";
+    secondSpade.style.color = "red";
+  }
 };
